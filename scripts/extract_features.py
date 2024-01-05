afa921ce99bae40aa5b756f30aeceaae306d0e24
PinetheApple <{ID}+{username}@users.noreply.github.com>
2024-01-05 16:49:13 +0530

    WIP script

---
+++ scripts/extract_features.py
@@ -0,0 +1,13 @@
+# script to generate feature set to match that of spambase.data to predict inputs based on model trained on that dataset
+def gen_feature_set(text) -> list:
+    wf_dict = {"make": 0, "address": 0, "all": 0, "3d": 0, "our": 0, "over": 0, "remove": 0, "internet": 0, "order": 0, "mail": 0, "receive": 0, "will": 0, "people": 0, "report": 0, "addresses": 0, "free": 0, "business": 0, "email": 0, "you": 0, "credit": 0, "your": 0, "font": 0, "000": 0,
+               "money": 0, "hp": 0, "hpl": 0, "george": 0, "650": 0, "lab": 0, "labs": 0, "telnet": 0, "857": 0, "data": 0, "415": 0, "85": 0, "technology": 0, "1999": 0, "parts": 0, "pm": 0, "direct": 0, "cs": 0, "meeting": 0, "original": 0, "project": 0, "re": 0, "edu": 0, "table": 0, "conference": 0}
+    cf_dict = {"cf_semicolon": 0, "cf_bracket": 0, "cf_sqbracket": 0,
+               "cf_exclamation": 0, "cf_dollar": 0, "cf_hash": 0}
+    crl_average = crl_longest = crl_total = 0
+    words = text.split(' ')
+    word_count = len(words)
+    return [val for val in wf_dict.values()] + [val for val in cf_dict.values()] + [crl_average, crl_longest, crl_total]
+
+
+print(gen_feature_set(''))
