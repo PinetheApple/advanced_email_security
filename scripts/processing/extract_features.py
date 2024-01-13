cbc3ab05615c1b5c2c52999b081d5b5a44f11d71
PinetheApple <pinespace889@gmail.com>
2024-01-13 09:39:39 +0530

    updated to classify based on predictions from multiple models

--- scripts/processing/extract_features.py
+++ scripts/processing/extract_features.py
@@ -1,7 +1,9 @@
 import re
-
+import numpy as np
 
 # script to generate feature set to match that of spambase.data to predict inputs based on model trained on that dataset
+
+
 def gen_feature_set(text) -> list:
     wf_dict = {"make": 0, "address": 0, "all": 0, "3d": 0, "our": 0, "over": 0, "remove": 0, "internet": 0, "order": 0, "mail": 0, "receive": 0, "will": 0, "people": 0, "report": 0, "addresses": 0, "free": 0, "business": 0, "email": 0, "you": 0, "credit": 0, "your": 0, "font": 0, "000": 0,
                "money": 0, "hp": 0, "hpl": 0, "george": 0, "650": 0, "lab": 0, "labs": 0, "telnet": 0, "857": 0, "data": 0, "415": 0, "85": 0, "technology": 0, "1999": 0, "parts": 0, "pm": 0, "direct": 0, "cs": 0, "meeting": 0, "original": 0, "project": 0, "re": 0, "edu": 0, "table": 0, "conference": 0}
@@ -25,4 +27,4 @@ def gen_feature_set(text) -> list:
         crl_average = crl_total / len(capitalized_words)
         crl_longest = max([len(word) for word in capitalized_words])
 
-    return [val for val in wf_dict.values()] + [val for val in cf_dict.values()] + [crl_average, crl_longest, crl_total]
+    return np.array([val for val in wf_dict.values()] + [val for val in cf_dict.values()] + [crl_average, crl_longest, crl_total])
